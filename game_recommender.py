11180d8dff55f9432f15f1efdb87ae0b9d4a9533
Xinsong Cui <andrewtsui0@gmail.com>
2023-03-12 16:14:32 -0400

    enable rest api

--- game_recommender.py
+++ game_recommender.py
@@ -5,6 +5,7 @@ from pandas import read_csv, Series, DataFrame, concat
 import pathlib
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+
 import requests
 import json
 
